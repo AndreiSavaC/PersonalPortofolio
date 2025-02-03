ec7b949d3798969993e3ecd4576efa253e8a7f7d
AndreiSavaC <savacristianandrei@yahoo.com>
2025-02-03 20:39:57 +0100

    test

--- send_notification.sh
+++ send_notification.sh
@@ -2,7 +2,9 @@
 
 source /home/PersonalPortofolio/.env
 
-MESSAGE="🚀 Deployment complete on VPS!"
+CURRENT_TIME=$(date "+%Y-%m-%d %H:%M:%S")
+
+MESSAGE="🚀 Deployment complete on VPS! ✅\n📅 Date: $CURRENT_TIME"
 
 curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \
      -d "chat_id=$CHAT_ID" \
