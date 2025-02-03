07dda2ee4b1aa68e231ce29c35f4e75ea19ae681
AndreiSavaC <savacristianandrei@yahoo.com>
2025-02-03 21:07:37 +0100

    Added favicon

--- send_notification.sh
+++ send_notification.sh
@@ -4,7 +4,9 @@ source /home/PersonalPortofolio/.env
 
 CURRENT_TIME=$(date "+%Y-%m-%d %H:%M:%S")
 
-MESSAGE="🚀 Deployment complete on VPS! ✅\n📅 Date: $CURRENT_TIME"
+MESSAGE="
+🚀 Deployment complete on VPS! ✅------
+📅 Date: $CURRENT_TIME"
 
 curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \
      -d "chat_id=$CHAT_ID" \
